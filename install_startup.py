e08520d7c767abf61d2196644a4428d4724990f4
h-takimoto <haruki.takimoto.a920@dirbato.co.jp>
2023-01-30 11:35:01 +0900

    Create install_startup.py

---
+++ install_startup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import subprocess
+
+# package install
+subprocess.run(['apt-get -y update'])
+subprocess.run(['apt-get -y install pkg-config libxml2-dev libxmlsec1 libxmlsec1-dev libxmlsec1-openssl gcc'])
+
+# pip install
+subprocess.run(['pip install xmlsec python3-saml Django==4.0.8'])
